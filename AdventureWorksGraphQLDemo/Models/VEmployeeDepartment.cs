e7e14e1ee13662c248517046c582224603e81e53
Matthew Sheluga <matthew.sheluga@gmail.com>
2025-10-28 13:51:32 -0400

    added in the DB via scaffolding

---
+++ AdventureWorksGraphQLDemo/Models/VEmployeeDepartment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdventureWorksGraphQLDemo.Models;
+
+[Keyless]
+public partial class VEmployeeDepartment
+{
+    [Column("BusinessEntityID")]
+    public int BusinessEntityId { get; set; }
+
+    [StringLength(8)]
+    public string? Title { get; set; }
+
+    [StringLength(50)]
+    public string FirstName { get; set; } = null!;
+
+    [StringLength(50)]
+    public string? MiddleName { get; set; }
+
+    [StringLength(50)]
+    public string LastName { get; set; } = null!;
+
+    [StringLength(10)]
+    public string? Suffix { get; set; }
+
+    [StringLength(50)]
+    public string JobTitle { get; set; } = null!;
+
+    [StringLength(50)]
+    public string Department { get; set; } = null!;
+
+    [StringLength(50)]
+    public string GroupName { get; set; } = null!;
+
+    public DateOnly StartDate { get; set; }
+}
