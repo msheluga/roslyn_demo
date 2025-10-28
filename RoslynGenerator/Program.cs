2a27cac1aedcaa97ab0403cc334380a900d238ff
Matthew Sheluga <matthew.sheluga@gmail.com>
2025-10-28 14:44:49 -0400

    need to build a actual project and get this looking better

--- RoslynGenerator/Program.cs
+++ RoslynGenerator/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using QueryGenerator;
+// See https://aka.ms/new-console-template for more information
+using RoslynGenerator.QueryGenerator;
+
+Console.WriteLine("Generate GraphQL Queries");
+
+var response = Console.ReadLine();
+
+switch (response)
+{
+    case "Y":
+        Console.WriteLine("----------------------------------------------");
+        Console.WriteLine("Generating GraphQl Class(es)...");
+        await QueryGenerator.GenerateQueryClass();
+
+        break;
+    default:
+        Console.WriteLine("Exiting Program...");
+        break;
+};
+
